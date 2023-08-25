6605ee639669dd2b37fe1271e6da947218d6dab0
Sebastian <hageneuer@gmail.com>
2023-08-25 13:30:27 +0200

    Moved funtion to external js
    I moved my functions to an external JS and tried to fix the problem of multiple textboxes

---
+++ js/BanquetScene.js
@@ -0,0 +1,37 @@
+/*
+The Banquet Scene - Interactive 3D View
+Copyright (c) 2023, Sebastian Hageneuer
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+function hideAllHotspotTextboxes() {
+  const collection = document.getElementsByClassName('text-box');
+  for (let i = 0; i < collection.length; i++) {
+      collection[i].style.visibility = "hidden";
+  }
+}
+
+function toggleTextboxVisibility(textbox_class) {
+  const collection = document.getElementsByClassName(textbox_class);
+  for (let i = 0; i < collection.length; i++) {
+    if (window.getComputedStyle(collection[i]).visibility === "hidden") {
+			hideAllHotspotTextboxes();
+      collection[i].style.visibility = "visible";
+    } else {
+      collection[i].style.visibility = "hidden";
+    }
+  }
+}
